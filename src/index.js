86b308067c17fe19f1ac303dc1dacc832bb2946f
Osmo Leppäniemi <oleppani@gmail.com>
2021-07-09 11:10:45 +0300

    eka

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const fetch = require('node-fetch')
+
+
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+
+
+app.listen({ port: process.env.PORT || 3001})
