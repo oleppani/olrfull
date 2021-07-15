a37fe94593c76dd1ddcc01dfc406067297a57a78
Osmo Leppäniemi <oleppani@gmail.com>
2021-07-15 22:35:06 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const path = require('path')
 
 const fetch = require('node-fetch')
-
+const request = require('request')
 
 function fetchaa(url){
     const sis = fetch(url, {
@@ -19,35 +19,33 @@ function fetchaa(url){
       })
       .catch(console.error);
 }
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true
+}));
 app.use(express.static('public'))
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
-app.post('*', async (req, res) => {
-      //tähän argumentin url lukeminen ja fetch annetusta urlista
-    //console.log('testiii')
-    //fetchaa('https://pokeapi.co/api/v2/pokemon').then((response) => 
-    //{
-      //  console.log(response)
-        //return(response)
-    //})
-    console.log('fetchii')
-    let re = await fetch('https://pokeapi.co/api/v2/pokemon')
-    console.log(re.text())
-    //.then((response) => 
-    /*{
-        console.log(response)
-        return(response)
-    })
-    .catch(console.error);
-    */
-   return re
-    //palautetaan fetchin tulos
-    //console.log(fe)
-    //return(fe)
-})
 
+app.post('/api', async (req, res) => {
+    
+    console.log('nyt')
+    console.log(req.headers.url)
+    
+      await request({
+        method: 'GET',
+        uri: req.headers.url,
+        headers: {}
+      }, function (error, response, body){
+        if(!error && response.statusCode === 200){
+            console.log(body)
+          res.json(body);
+        }
+      })
+    
+      
+})
 
 
 app.listen({ port: process.env.PORT || 3001})
